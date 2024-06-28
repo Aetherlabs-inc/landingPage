f6ab10300071c42ee0ce3b3c46e87ec0676b8005
rashodk <rmanamperiko@mun.ca>
2024-06-28 17:04:09 -0230

    chore: Update image paths in LandingPage components

--- src/LandingPage/Features/index.tsx
+++ src/LandingPage/Features/index.tsx
@@ -3,14 +3,13 @@ import React from "react";
 
 const Features = () => {
     return (
-        <section className="py-16 bg-gray-100">
+        <section className="py-16">
             <div className="overflow-hidden py-24 sm:py-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         <div className="lg:pr-8 lg:pt-4">
                             <div className="lg:max-w-lg">
-                                <h2 className="text-base font-semibold leading-7 text-indigo-600">Revolutionize Art Authentication</h2>
-                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Key Features</p>
+                                <h2 className="text-4xl font-semibold leading-8 text-gray-600">Revolutionize Art Authentication</h2>
                                 <p className="mt-6 text-lg leading-8 text-gray-600">Discover how our platform ensures the authenticity and security of your artworks.</p>
                                 <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                                     <div className="relative pl-9">
