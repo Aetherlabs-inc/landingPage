cd902f960350af585ceb2b813a162eeab4e491c5
rashodk <rmanamperiko@mun.ca>
2024-12-24 04:08:34 -0330

    Add Typeform embed integration and update package dependencies

---
+++ app/(Main)/survey/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Widget } from '@typeform/embed-react'
+
+export default function MyPage() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <Widget id="IRknsunB" className="w-full h-full" />
+        </div>
+    )
+}
