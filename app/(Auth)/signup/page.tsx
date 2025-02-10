e38a8e83fcbebf1746ae4a8553b2ed5b822d2902
rashodk <rmanamperiko@mun.ca>
2025-02-10 16:15:07 -0330

    Add InputOTP component and integrate input-otp library; update package.json and pnpm-lock.yaml

--- app/(Auth)/signup/page.tsx
+++ app/(Auth)/signup/page.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+
 import { SignupForm } from "@/src/auth/signup/signup-form"
+
 
 export default function SignupPage() {
     return (
--- src/auth/signup/signup-form.tsx
+++ src/auth/signup/signup-form.tsx
@@ -1,14 +1,23 @@
-'use client';
+'use client'
 import React, { useState } from "react"
-import { cn } from "@/lib/utils"
+import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { signIn } from 'aws-amplify/auth';
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils"
+import Image from "next/image";
+
+
 import { BsEyeSlashFill, BsFillEyeFill } from "react-icons/bs";
 import { useRouter } from "next/navigation";
-import LoadingScreen from "@/components/LoadingScreen";
-import Image from "next/image";
+import { signUp, confirmSignUp } from 'aws-amplify/auth';
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp"
+
 
 export function SignupForm({
     className,
@@ -18,149 +27,182 @@ export function SignupForm({
     const router = useRouter();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [otp, setOtp] = useState("");
     const [isVisible, setIsVisible] = useState(false);
-    const [error, setError] = useState<string>(""); // Error state with explicit string type
-    const [loading, setLoading] = useState(false); // Loading state
 
     const toggleVisibility = () => setIsVisible(!isVisible);
 
-    const handleSignup = async () => {
-        setError(""); // Reset error before Signup attempt
-        setLoading(true); // Set loading state to true
+    const [ConfirmSginUp, setConfirmSginUp] = useState(false);
 
+    const handlesingUp = async () => {
         try {
-            const { isSignedIn, nextStep } = await signIn({ username: email, password });
+            const { isSignUpComplete, userId, nextStep } = await signUp({
+                username: email,
+                password,
+                options: {
+                    userAttributes: {
+                        email,
+                    },
+                    // optional
+                    autoSignIn: true // or SignInOptions e.g { authFlowType: "USER_SRP_AUTH" }
+                }
+            });
 
-            if (isSignedIn) {
-                router.push("/");
-            } else {
-                console.log('next step', nextStep);
+            if (isSignUpComplete) {
+                console.log('user has been signed up');
+                // redirect to the next step
+                router.push('/Signup');
+                return;
             }
 
-            if (nextStep) {
+            else if (nextStep) {
                 console.log('next step is required:', nextStep);
-                if (nextStep.signInStep === 'DONE') {
-                    router.push('/dashboard');
+                // handle the next steps
+                if (nextStep.signUpStep === 'CONFIRM_SIGN_UP') {
+                    // you need to get the confirmation code to confirm the sign up
+                    // you can do this by redirecting the user to the confirm sign up page
+                    setConfirmSginUp(true);
+                    //rerender the page
+                    // router.push('/Signup');
                     return;
                 }
             }
-        } catch (error: unknown) {
-            console.log('error signing in', error);
 
-            if (error instanceof Error && 'code' in error) {
-                const authError = error as { code: string };
-                switch (authError.code) {
-                    case 'UserNotFoundException':
-                        setError("Account doesn't exist");
-                        break;
-                    case 'NotAuthorizedException':
-                        setError("Incorrect username or password");
-                        break;
-                    default:
-                        setError("An unknown error occurred, please try again");
-                        break;
-                }
-            } else {
-                setError("An unexpected error occurred");
-            }
-        } finally {
-            setLoading(false); // Set loading state to false once the process is complete
+
+            console.log(userId);
+        } catch (error) {
+            console.log('error signing up:', error);
         }
-    };
-
-    if (loading) {
-        return (
-            <LoadingScreen />
-        );
     }
 
+
+    const handleConfirmSignUp = async () => {
+        try {
+
+            await confirmSignUp({ username: email, confirmationCode: otp });
+
+            console.log('user has been confirmed');
+
+            // redirect to the next step
+            router.push('/Signup');
+
+        } catch (error) {
+            console.log('error signing in:', error);
+        }
+    }
     return (
-        <form className={cn("flex flex-col gap-6", className)} {...props}>
-            <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Signup to your account</h1>
-                <p className="text-balance text-sm text-muted-foreground">
-                    Enter your email below to Signup to your account
-                </p>
-            </div>
-            <div className="grid gap-6">
-                <div className="grid gap-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        placeholder="m@example.com"
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                </div>
-                <div className="grid gap-2">
-                    <div className="flex items-center">
-                        <Label htmlFor="password">Password</Label>
-                        <a
-                            href="#"
-                            className="ml-auto text-sm underline-offset-4 hover:underline"
-                        >
-                            Forgot your password?
+        <>
+            {!ConfirmSginUp ? (
+                <form className={cn("flex flex-col gap-6", className)} {...props}>
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1 className="text-2xl font-bold">Everything Starts Here!</h1>
+                        <p className="text-balance text-sm text-muted-foreground">
+                            Sign up to get started with Aether
+                        </p>
+                    </div>
+                    <div className="grid gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <a
+                                    href="#"
+                                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                                >
+                                    Forgot your password?
+                                </a>
+                            </div>
+                            <div className="relative w-full flex items-center">
+                                <Input
+                                    type={isVisible ? "text" : "password"}
+                                    placeholder="Enter your password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className="w-full h-12 rounded-xl pr-12" // Ensure there is padding to not overlap the icon
+                                />
+                                <Button
+                                    onClick={toggleVisibility}
+                                    className="absolute right-0 inset-y-0 h-12 w-12 flex items-center justify-center rounded-xl"
+                                    aria-label="Toggle password visibility"  // Accessibility improvement
+                                    variant="ghost"
+                                >
+                                    {isVisible ? <BsFillEyeFill /> : <BsEyeSlashFill />}
+                                </Button>
+                            </div>
+                        </div>
+
+                        <Button type="submit" className="w-full" onClick={handlesingUp}>
+                            Create Account
+                        </Button>
+                        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                                Or continue with
+                            </span>
+                        </div>
+                        <div className="grid gap-2">
+                            <Button variant="outline" className="w-full">
+                                <Image
+                                    src="/apple-logo.png"
+                                    alt="Apple logo"
+                                    width={20}
+                                    height={20}
+                                    className="mr-2"
+                                />
+                                Signup with Apple
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                <Image
+                                    src="/google.png"
+                                    alt="Apple logo"
+                                    width={20}
+                                    height={20}
+                                    className="mr-2"
+                                />
+                                Signup with Google
+                            </Button>
+                        </div>
+
+                    </div>
+                    <div className="text-center text-sm">
+                        Don&apos;t have an account?{" "}
+                        <a href="/login" className="underline underline-offset-4">
+                            Login
                         </a>
                     </div>
-                    <div className="relative w-full flex items-center">
-                        <Input
-                            type={isVisible ? "text" : "password"}
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full h-12 rounded-xl pr-12" // Ensure there is padding to not overlap the icon
-                        />
-                        <Button
-                            onClick={toggleVisibility}
-                            className="absolute right-0 inset-y-0 h-12 w-12 flex items-center justify-center rounded-xl"
-                            aria-label="Toggle password visibility"  // Accessibility improvement
-                            variant="ghost"
-                        >
-                            {isVisible ? <BsFillEyeFill /> : <BsEyeSlashFill />}
-                        </Button>
-                    </div>
-                </div>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
-                <Button type="submit" className="w-full" onClick={handleSignup} disabled={loading}>
-                    Signup
-                </Button>
-                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                        Or continue with
-                    </span>
-                </div>
-                <div className="grid gap-2">
-                    <Button variant="outline" className="w-full">
-                        <Image
-                            src="/apple-logo.png"
-                            alt="Apple logo"
-                            width={20}
-                            height={20}
-                            className="mr-2"
-                        />
-                        Signup with Apple
-                    </Button>
-                    <Button variant="outline" className="w-full">
-                        <Image
-                            src="/google.png"
-                            alt="Apple logo"
-                            width={20}
-                            height={20}
-                            className="mr-2"
-                        />
-                        Signup with Google
-                    </Button>
-                </div>
+                </form>) : (
+                <Card className="p-6 md:w-96 flex flex-col justify-center items-center">
+                    <InputOTP maxLength={6}
+                        value={otp}
+                        onChange={(otp) => setOtp(otp)}
+                        onComplete={handleConfirmSignUp}>
+                        <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                    </InputOTP>
 
-            </div>
-            <div className="text-center text-sm">
-                Already have an account?{" "}
-                <a href="/login" className="underline underline-offset-4">
-                    Login
-                </a>
-            </div>
-        </form>
-    )
+                </Card>
+            )
+            }
+        </>
+    );
 }
+
+
